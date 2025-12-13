0fff75c98361d66d1d00f90648e6524640cad6ed
Ueli <ueli.honauer@stud.fhgr.ch>
2025-12-13 10:22:35 +0100

    merged GUI and Console mode
    either mode can be started by using command line arguments

--- Python/main.py
+++ Python/main.py
@@ -34,41 +34,66 @@ image_loader = Image_Loader.Image_Loader(config['settings']['picture_path'], int
 image_processor = Image_Processing.Image_Processing(colors, ranges)
 logger = Logger.Logger(config['logging']['path'])
 
+def run_console_mode(selected_input_mode):
+    while True:               
+        if selected_input_mode == 'camera':
+            print("Starting camera mode. Press 'q' to quit.")
+            while True:
+                image = image_loader.load_camera_image()
+                image, shapes = image_processor.process_images(image)
+                logger.log_detection(shapes)
+                cv2.imshow('Image', image)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+        else:
+            print("Starting image mode. Press any key to proceed through images.")
+            images = image_loader.load_folder_images()
+
+            for image in images:
+                image, shapes = image_processor.process_images(image)
+                logger.log_detection(shapes)
+                cv2.imshow('Image', image)
+                cv2.waitKey(0)
+
+        cv2.destroyAllWindows() 
+        
+        print("Which mode do want to select:\n[0] Default\n[1] Image\n[2] Camera\n[3] Quit")
+        selection = int(input())
+        if selection == 0:
+            pass
+        elif selection == 1:
+            selected_input_mode = 'image'
+        elif selection == 2:
+            selected_input_mode = 'camera'
+        elif selection == 3:
+            break
+        else:
+            print("Invalid selection, defaulting to 'image' mode.")
+
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = ShapeDetectorApp.ShapeDetectorApp(image_loader, image_processor, logger)
-    window.show()
-    sys.exit(app.exec())
+    if len(sys.argv) > 1:
+        run_mode = sys.argv[1].upper()
+        if run_mode != 'CONSOLE' and run_mode != 'GUI':
+            run_mode = config['settings']['run_mode']
+    else:
+        run_mode = config['settings']['run_mode']    
+        
+    if run_mode == 'GUI':
+        app = QApplication(sys.argv)
+        window = ShapeDetectorApp.ShapeDetectorApp(image_loader, image_processor, logger)
+        window.show()
+        sys.exit(app.exec())
 
-# while True:
-#     selected_mode = config['settings']['mode']
-#     print("Which mode do want to select:\n[0] Default\n[1] Image\n[2] Camera")
-#     selection = int(input())
-#     if selection == 0:
-#         pass
-#     elif selection == 1:
-#         selected_mode = 'image'
-#     elif selection == 2:
-#         selected_mode = 'camera'
-#     else:
-#         print("Invalid selection, defaulting to 'image' mode.")
-    
-#     if selected_mode == 'camera':
-#         print("Starting camera mode. Press 'q' to quit.")
-#         while True:
-#             image = image_loader.load_camera_image()
-#             image, shapes = image_processor.process_images(image)
-#             logger.log_detection(shapes)
-#             cv2.imshow('Image', image)
-#             if cv2.waitKey(1) & 0xFF == ord('q'):
-#                 break
-#     else:
-#         print("Starting image mode. Press any key to proceed through images.")
-#         images = image_loader.load_folder_images()
-
-#         for image in images:
-#             image, shapes = image_processor.process_images(image)
-#             logger.log_detection(shapes)
-#             cv2.imshow('Image', image)
-#             cv2.waitKey(0)
-#     cv2.destroyAllWindows() 
+    else:
+        if len(sys.argv) > 2:
+            input_mode = sys.argv[2].upper()
+            if input_mode == 'IMAGE':
+                selected_input_mode = 'image'
+            elif input_mode == 'CAMERA':
+                selected_input_mode = 'camera'
+            else:
+                print("Invalid mode argument, defaulting to config setting.")
+                selected_input_mode = config['settings']['mode']
+        else:
+            selected_input_mode = config['settings']['mode']
+        run_console_mode(selected_input_mode)
